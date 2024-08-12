5de8eed9b698fe52ddbe8afd1a6cd86f75f72a18
butterfly930 <elsarrustaj@gmail.com>
2024-08-12 16:57:16 +0200

    recommit

--- scripthomepage.js
+++ scripthomepage.js
@@ -87,31 +87,29 @@ $(document).ready(function () {
     // Show the loader initially
     $('#slide-carousel').html('<div class="loader2"></div>');
 
-    // Fetch the quotes after a 3-second delay
+    // Fetch the videos after a 2-second delay
     setTimeout(function () {
         $.ajax({
             url: 'https://smileschool-api.hbtn.info/popular-tutorials',
             method: 'GET',
             success: function (data) {
-                let carouselInner = '';
-                data.forEach((quote, index) => {
-                    carouselInner += `
+                let carouselItems = '';
+                data.forEach((video, index) => {
+                    carouselItems += `
             <div class="carousel-item ${index === 0 ? 'active' : ''}">
               <div class="row mx-auto align-items-center">
-                <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
-                  <img
-                    src="${quote.pic_url}"
-                    class="d-block align-self-center"
-                    alt="Profile picture of ${quote.name}"
-                  />
-                </div>
-                <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
-                  <div class="quote-text">
-                    <p class="text-white">
-                      « ${quote.text}
-                    </p>
-                    <h4 class="text-white font-weight-bold">${quote.name}</h4>
-                    <span class="text-white">${quote.title}</span>
+                <div class="col-12 col-sm-6 col-lg-4">
+                  <div class="video-card">
+                    <img
+                      src="${video.thumb_url}"
+                      class="img-fluid"
+                      alt="Thumbnail of ${video.title}"
+                    />
+                    <div class="video-info">
+                      <h4>${video.title}</h4>
+                      <p>${video['sub-title']}</p>
+                      <p class="text-muted">${video.author}</p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -119,10 +117,10 @@ $(document).ready(function () {
                 });
 
                 // Update the carousel with the fetched data
-                $('#videos-carousel').html(`
+                $('#slide-carousel').html(`
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
-              ${carouselInner}
+              ${carouselItems}
             </div>
             <a
               class="carousel-control-prev arrow-left"
@@ -132,7 +130,7 @@ $(document).ready(function () {
             >
               <img
                 src="images/arrow_black_left.png"
-                alt="Quote Previous"
+                alt="Video Previous"
                 aria-hidden="true"
               />
               <span class="sr-only">Previous</span>
@@ -145,7 +143,7 @@ $(document).ready(function () {
             >
               <img
                 src="images/arrow_black_right.png"
-                alt="Quote Next"
+                alt="Video Next"
                 aria-hidden="true"
               />
               <span class="sr-only">Next</span>
@@ -154,12 +152,11 @@ $(document).ready(function () {
         `);
             },
             error: function (error) {
-                console.error('Error fetching quotes:', error);
+                console.error('Error fetching videos:', error);
             },
         });
-    }, 2000); // Delay of 3000ms (3 seconds)
+    }, 2000); // Delay of 2000ms (2 seconds)
 });
-
 
 
 
@@ -174,31 +171,29 @@ $(document).ready(function () {
     // Show the loader initially
     $('#videos-carousel').html('<div class="loader3"></div>');
 
-    // Fetch the quotes after a 3-second delay
+    // Fetch the videos after a 2-second delay
     setTimeout(function () {
         $.ajax({
             url: 'https://smileschool-api.hbtn.info/latest-videos',
             method: 'GET',
             success: function (data) {
-                let carouselInner = '';
-                data.forEach((quote, index) => {
-                    carouselInner += `
+                let carouselItems = '';
+                data.forEach((video, index) => {
+                    carouselItems += `
             <div class="carousel-item ${index === 0 ? 'active' : ''}">
               <div class="row mx-auto align-items-center">
-                <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
-                  <img
-                    src="${quote.pic_url}"
-                    class="d-block align-self-center"
-                    alt="Profile picture of ${quote.name}"
-                  />
-                </div>
-                <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
-                  <div class="quote-text">
-                    <p class="text-white">
-                      « ${quote.text}
-                    </p>
-                    <h4 class="text-white font-weight-bold">${quote.name}</h4>
-                    <span class="text-white">${quote.title}</span>
+                <div class="col-12 col-sm-6 col-lg-4">
+                  <div class="video-card">
+                    <img
+                      src="${video.thumb_url}"
+                      class="img-fluid"
+                      alt="Thumbnail of ${video.title}"
+                    />
+                    <div class="video-info">
+                      <h4>${video.title}</h4>
+                      <p>${video['sub-title']}</p>
+                      <p class="text-muted">${video.author}</p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -209,7 +204,7 @@ $(document).ready(function () {
                 $('#videos-carousel').html(`
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
-              ${carouselInner}
+              ${carouselItems}
             </div>
             <a
               class="carousel-control-prev arrow-left"
@@ -219,7 +214,7 @@ $(document).ready(function () {
             >
               <img
                 src="images/arrow_black_left.png"
-                alt="Quote Previous"
+                alt="Video Previous"
                 aria-hidden="true"
               />
               <span class="sr-only">Previous</span>
@@ -232,7 +227,7 @@ $(document).ready(function () {
             >
               <img
                 src="images/arrow_black_right.png"
-                alt="Quote Next"
+                alt="Video Next"
                 aria-hidden="true"
               />
               <span class="sr-only">Next</span>
@@ -241,10 +236,10 @@ $(document).ready(function () {
         `);
             },
             error: function (error) {
-                console.error('Error fetching quotes:', error);
+                console.error('Error fetching videos:', error);
             },
         });
-    }, 2000); // Delay of 3000ms (3 seconds)
+    }, 2000); // Delay of 2000ms (2 seconds)
 });
 
 
